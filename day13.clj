4230b1f6c7d81934c96b57d170cc22091add1e72
Max Gerlach <max@ada.support>
2021-12-14 16:21:13 +0100

    Now day 13

--- day13.clj
+++ day13.clj
@@ -27,79 +27,80 @@ fold along x=5")
 
 (defn make-grid [s]
   (reduce (fn [acc l]
-            (assoc acc (utils/split-commas-to-ints l) true))
-            {}
-            (str/split-lines s)))
+                     (assoc acc (utils/split-commas-to-ints l) true))
+                     {}
+                     (str/split-lines s)))
           
-
-(defn grid-width [g]
-  (apply max (map first (keys g))))
-
-(defn grid-height [g]
-  (apply max (map second (keys g))))
 
 (defn print-grid [g]
-  (doseq [y (range (inc (grid-height g)))]
+  (let [width (apply max (map first (keys g)))
+        height (apply max (map second (keys g)))]
+  (doseq [y (range (inc height))]
     (println
      (apply str
-            (for [x (range (inc (grid-width g)))]
-              (if (contains? g (list x y)) "*" " "))))))
-          
+            (for [x (range (inc width))]
+              (if (contains? g (list x y)) "#" " ")))))))
+  
 
-;; (defn split-grid [g x-or-y val]
-;;   (->>
-;;    (group-by (fn [[pos _]] (> (x-or-y pos) val)) g)
-;;    (vals)
-;;   (map #(into {} %))))
+(defn fold-grid-y
+  "fold grid G along FOLD-LINE"
+  [g fold-line]
+  (let [max-y (+ fold-line fold-line)
+        grid (reduce-kv (fn [g [x y] v]
+                          (let [y' (if (> y fold-line)
+                                          (- max-y y)
+                                          y)]
+                           
+                            (assoc g (list x y') v)))
+                        {} g)]
+    grid))
 
-(defn fold-grid
-  "fold grid G along X-OR-Y-IDX which is 0 for x and 1 for y, and the
-  value VAL"
-  [g x-or-y-idx val]
-  (let [max-dim (apply max (map #(nth % x-or-y-idx) (keys g)))
-        other {0 1 1 0}]
-    (reduce-kv
-     (fn [g k v]
-       (let [x-or-y (nth k x-or-y-idx)
-             x-or-y' (if (>= x-or-y val)
-                       (- max-dim x-or-y)
-                       x-or-y)
-             other-x-or-y-idx (get other x-or-y-idx)
-             other-x-or-y (nth k other-x-or-y-idx)
-             new-coords (-> ['x 'y]
-                            (assoc x-or-y-idx x-or-y')
-                            (assoc other-x-or-y-idx other-x-or-y)
-                            )]
-         (assoc g new-coords v)))
-     {} g)))
+  (defn fold-grid-x
+  "fold grid G along FOLD-LINE"
+  [g fold-line]
+  (let [max-x (+ fold-line fold-line)
+        grid (reduce-kv (fn [g [x y] v]
+                          (let [x' (if (> x fold-line)
+                                          (- max-x x)
+                                          x)]
+                            (assoc g (list x' y) v)))
+                        {} g)]
+    grid))
 
-
+  
 (->  (make-grid +pp+)
-     (fold-grid 1 7)
-     (fold-grid 0 5)
+     (fold-grid-y 7)
+     (fold-grid-x 5)
      (print-grid))
 
 ;;; part 1
 (def +puzzle-grid+ (slurp "day13.input"))
 (-> 
  (make-grid +puzzle-grid+)
- (fold-grid 0 655)
+ (fold-grid-x 655)
  (count));; => 781
 
 
 ;;; part 2
+(with-out-str
 (->  (make-grid +puzzle-grid+)
-     (fold-grid 0 655)
-     (fold-grid 1 447)
-     (fold-grid 0 327)
-     (fold-grid 1 223)
-     (fold-grid 0 163)
-     (fold-grid 1 111)
-     (fold-grid 0 81)
-     (fold-grid 1 55)
-     (fold-grid 0 40)
-     (fold-grid 1 27)
-     (fold-grid 1 13)
-     (fold-grid 1 6)
-     (print-grid))
-
+     (fold-grid-x 655)
+     (fold-grid-y 447)
+     (fold-grid-x 327)
+     (fold-grid-y 223)
+     (fold-grid-x 163)
+     (fold-grid-y 111)
+     (fold-grid-x 81)
+     (fold-grid-y 55)
+     (fold-grid-x 40)
+     (fold-grid-y 27)
+     (fold-grid-y 13)
+     (fold-grid-y 6)
+     (print-grid))) ;; =>
+;;"###  #### ###   ##   ##    ## ###  ### 
+;; #  # #    #  # #  # #  #    # #  # #  #
+;; #  # ###  #  # #    #       # #  # ### 
+;; ###  #    ###  #    # ##    # ###  #  #
+;; #    #    # #  #  # #  # #  # #    #  #
+;; #    #### #  #  ##   ###  ##  #    ### 
+"
