c3489e1c8bcb72a23e3635fab09b94ac986611f5
Max Gerlach <max@ada.support>
2021-12-17 13:04:16 +0100

    finally works

--- day15.clj
+++ day15.clj
@@ -49,20 +49,29 @@
 
 (defn print-grid
   ([grid]
+   (print-grid grid #{}))
+
+  ([grid hl]
    (let [wh (Math/sqrt (count (keys grid)))]
-     (print-grid grid wh wh)))
-   
-  ([grid w h]
-   (println
-    (apply str
-           (interpose "\n"
-                      (for [y (range h)]
-                        (apply str (for [x (range w)]
-                                     (format "%x" (get grid [x y] 0)))))))
-    "\n")))
+     (print-grid grid hl wh wh)))
+
+  ([grid hl w h]
+   (doseq [y (range h)]
+         (doseq [x (range w)]
+           (print
+            (if (contains? (set hl) [x y])
+              (utils/with-ansi :bright
+                (format "%x" (get grid [x y] 0)))
+              (format "%x" (get grid [x y] 0)))))
+         (print "\n"))))
 
 
-
+(defn get-path [dist goal]
+  (loop [cur (get dist goal)
+         acc (list goal)]
+    (if-let [next (get dist (:parent cur))]
+      (recur next (conj acc (:parent cur)))
+      acc)))
 
 
 (defn neighbours [gridmap [x y]]
@@ -78,52 +87,128 @@
   (gridmap pos))
 
     
-(defn djikstra [graph start goal neighbours cost]
+(defn djikstra [graph start neighbours cost]
   (let [dist (atom (-> (->> (keys graph)
                             (map (fn [pos]
                                    [pos {:cost Integer/MAX_VALUE
                                          :parent nil}]))
                             (into {}))
                        (assoc start {:cost 0 :parent nil})))
-        pq (PriorityQueue. #(compare (:cost (second %1))
-                                     (:cost (second %2))))]
+        pq (PriorityQueue. #(compare ^int (:cost (second %1))
+                                     ^int (:cost (second %2))))]
 
-    ;; (def dist dist) (def pq pq)
-    
     (doseq [[pos {cost :cost}] (seq @dist)]
       (.add pq [pos cost]))
 
     (loop [[cur curcost] [start 0]]
-          
-          (doseq [n (neighbours cur)
-                  :let [ntotal (+ curcost (cost n))]]
-            (let [{:keys [cost]} (@dist n)]
-              (when (< ntotal cost)
-                (swap! dist assoc n {:cost ntotal :parent cur})
-                (.remove pq [n cost])
-                (.add pq [n ntotal]))))
-          
-          (if-let [next-cur-cost (.poll pq)]
-            (recur next-cur-cost)
-              @dist))))
 
-;;(def +p+ (slurp "day15.input"))
+      (doseq [n (neighbours cur)
+              :let [ntotal (+ curcost (cost n))
+                    {cost :cost} (@dist n)]]
+        (when (< ntotal cost)
+            (swap! dist assoc n {:cost ntotal :parent cur})
+            (.remove pq [n cost])
+            (.add pq [n ntotal])))
+
+      (if-let [next-cur-cost (.poll pq)]
+        (recur next-cur-cost)
+        @dist))))
+
+
+
 
 (time
  (let [+p+ (slurp "day15.input")
-       gridmap (repeat-grid (grid->map +p+) 5)
-      [dim-x dim-y] (grid-dimension gridmap)
+       ;;gridmap (repeat-grid (grid->map +p+) 5)
+       gridmap (grid->map +p+)
+       
+       [dim-x dim-y] (grid-dimension gridmap)
+       endnode [(dec dim-x) (dec dim-y)]
       _ (do
           (def gridmap gridmap))
       dist (djikstra
             gridmap
             [0 0]
-            [(dec dim-x) (dec dim-y)]
             (partial neighbours gridmap)
-            (partial cost gridmap))]
-  (def dist dist)
-  (dist [(dec dim-x) (dec dim-y)]))
-)
+            (partial cost gridmap)
+            )]
+   (print-grid gridmap (get-path dist endnode))
+
+  endnode
+))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn cost-a* [gridmap [end-x end-y] [x y :as pos]]
+  (+ (Math/abs (- end-x x))
+     (Math/abs (- end-y y))
+     (gridmap pos)))
+
+    
+(defn a* [graph start neighbours goal cost pathcost]
+  (let [dist (atom (-> (->> (keys graph)
+                            (map (fn [pos]
+                                   [pos {:cost Integer/MAX_VALUE
+                                         :pathcost 0
+                                         :parent nil}]))
+                            (into {}))
+                       (assoc start {:cost 0 :parent nil :pathcost 0})))
+        pq (PriorityQueue. #(compare ^int (second %1)
+                                     ^int (second %2)))]
+
+    (loop [[cur curcost] [start 0]
+           visited (set [start])]
+      ;;(prn 'looking-at cur (@dist cur))
+
+      (doseq [n (neighbours cur)
+              :when (not (contains? visited n))
+              :let [ncost (+ curcost (cost n))
+                    {cost :cost} (@dist n)]]
+        (when (< ncost cost)
+          (swap! dist assoc n {:cost ncost
+                               :pathcost (+ (:pathcost (@dist cur))
+                                            (pathcost n))
+                               :parent cur})
+          (when (.contains pq [n cost])
+            (.remove pq [n cost]))
+          (.add pq [n ncost])))
+
+      (let [[next-cur _ :as next] (.poll pq)]
+        (if (= next-cur goal)
+          (do (def dist @dist)
+              (@dist goal))
+          (recur next (conj visited cur)))))))
+
+  
+;;(def +p+ (slurp "day15.input"))
+
+(time
+ (let [+p+ (slurp "day15.input")
+       gridmap (grid->map +p+)
+       gridmap (repeat-grid (grid->map +p+) 5)
+       [dim-x dim-y] (grid-dimension gridmap)
+       endnode [(dec dim-x) (dec dim-y)]
+      _ (do
+          (def gridmap gridmap))
+      goalnode (a*
+            gridmap
+            [0 0]
+            (partial neighbours gridmap)
+            endnode
+            (partial cost gridmap #_endnode) ;; i get the wrong result with the heuristic?
+            (partial cost gridmap)
+            )]
+   (def endnode endnode)
+   (prn goalnode)
+   (spit "/tmp/path.txt"
+         (with-out-str
+           (print-grid gridmap (get-path dist endnode))))
+  goalnode
+));; => {:cost 2925, :pathcost 2925, :parent [499 498]}
 
 
 
--- utils.clj
+++ utils.clj
@@ -7,3 +7,47 @@
 (defn split-commas-to-ints [line]
 	(for [num (str/split (str/replace line #"\s" "") #",")]
 		(parse-int num)))
+
+(def contains-not? (complement contains?))
+
+(defn includes? [lst elt]
+  (some #{elt} lst))
+
+(def ANSI-CODES
+  {:reset              "[0m"
+   :bright             "[1m"
+   :blink-slow         "[5m"
+   :underline          "[4m"
+   :underline-off      "[24m"
+   :inverse            "[7m"
+   :inverse-off        "[27m"
+   :strikethrough      "[9m"
+   :strikethrough-off  "[29m"
+
+   :default "[39m"
+   :white   "[37m"
+   :black   "[30m"
+   :red     "[31m"
+   :green   "[32m"
+   :blue    "[34m"
+   :yellow  "[33m"
+   :magenta "[35m"
+   :cyan    "[36m"
+
+   :bg-default "[49m"
+   :bg-white   "[47m"
+   :bg-black   "[40m"
+   :bg-red     "[41m"
+   :bg-green   "[42m"
+   :bg-blue    "[44m"
+   :bg-yellow  "[43m"
+   :bg-magenta "[45m"
+   :bg-cyan    "[46m"
+   })
+
+(defn ansi
+  [code]
+  (str \u001b (get ANSI-CODES code (:reset ANSI-CODES))))
+
+(defn with-ansi [code s]
+  (str (ansi code) s (ansi :reset)))
